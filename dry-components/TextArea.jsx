297ddc17c78292b95504d8cda27751ad81ef34f3
hrithikscube <hrithik@scube.me>
2024-09-17 09:11:06 +0530

    common components

---
+++ dry-components/TextArea.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const TextArea = ({ value, name, type, handleChange, width, label, rows }) => {
+    return (
+        <div className={`${width || 'w-full'}`}>
+            <textarea
+                rows={rows || 5}
+                placeholder={label}
+                type={type}
+                name={name}
+                value={value}
+                onChange={handleChange}
+                className='py-2 px-3 border resize-none border-[#808080]/50 w-full lg:text-base text-sm outline-[#121212] rounded'
+            />
+
+        </div>
+    )
+}
+
+export default TextArea
